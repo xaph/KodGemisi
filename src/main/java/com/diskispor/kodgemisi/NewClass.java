20995418a0fb175be0c38ca18ef2952fe6dbf8eb
Mesut <mst@mesutyakut.com>
2014-12-31 12:07:38 +0200

    changed hide

---
+++ src/main/java/com/diskispor/kodgemisi/NewClass.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.diskispor.kodgemisi;
+
+/**
+ *
+ * @author Mesut
+ */
+public class NewClass {
+    public void asd(){
+        System.out.println("asd class");
+    }
+}
